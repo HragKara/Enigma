322fc7bad782a867eacc3e7bc66b4aa96391eb06
HragKara <46092072+HragKara@users.noreply.github.com>
2019-12-08 00:15:53 -0800

    Update Url
    Forgot to put dollar signs

--- apps/enigma/urls.py
+++ apps/enigma/urls.py
@@ -3,6 +3,6 @@ from . import views
                     
 urlpatterns = [
     url(r'^$', views.index),
-    url(r'^enigma-pricing', views.pricing),
-    url(r'^buy-enigma-platinum', views.platinum)
+    url(r'^enigma-pricing$', views.pricing),
+    url(r'^buy-enigma-platinum$', views.platinum)
 ]
